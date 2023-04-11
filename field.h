6e88136b1d335c42bc3d888c47c524523841b827
Mikhail <mikhail03.yershov@gmail.com>
2023-04-11 14:18:30 +0300

    Added field.h + cell.h

--- field.h
+++ field.h
@@ -2,13 +2,38 @@
 // Created by syten on 07.04.23.
 //
 
-#pragma once
+#ifndef UNIVERSAL_GAMING_FIELD_FIELD_H
+#define UNIVERSAL_GAMING_FIELD_FIELD_H
 
-//#ifndef UNIVERSAL_GAMING_FIELD_FIELD_H
-//#define UNIVERSAL_GAMING_FIELD_FIELD_H
+#include <vector>
+#include <set>
+#include <string>
 
 class Field {
+public:
+    Field() = delete;
+    Field(std::size_t width, std::size_t height);
+    Field(std::vector<std::vector<bool>> matrix);
+
+    int changeSize(int left, int right, int below, int avard);
+
+    int addObject(std::size_t x, std::size_t y, GameObject obj);
+    void deteleObject(std::size_t x, std::size_t y);
+
+
+
+    class Cell {
+    public:
+        bool isAvalForObj();
+
+    private:
+        bool is_aval_for_obj;
+        static std::set<std::string> possible_tags;
+    };
+
+private:
+
 
 };
 
-//#endif //UNIVERSAL_GAMING_FIELD_FIELD_H
+#endif //UNIVERSAL_GAMING_FIELD_FIELD_H
