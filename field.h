9dad8147d2f9c3d1d7cbe5d637228b56f17ae90f
Mikhail <mikhail03.yershov@gmail.com>
2023-04-18 21:41:00 +0300

    Field:
    header file:
    1) Added rotation
    2) fixed methods of getting objects by tag

--- field.h
+++ field.h
@@ -11,38 +11,65 @@
 #include <string>
 #include <unordered_set>
 
+//struct GamePreset {
+//public:
+//    Field field;
+//    std::string presetName;
+//};
+
 class Field {
+
 public:
+
+    enum RotationDirection {
+        CLOCKWISE,
+        COUNTERCLOCKWISE
+    };
+
+private:
+
+    class Cell {
+
+    public:
+
+        static std::unordered_set<std::string> possibleTags;
+
+    private:
+
+        bool is_aval_for_obj;
+
+    };
+
+public:
+
     Field() = delete;
     Field(std::size_t width, std::size_t height);
     Field(std::vector<std::vector<bool>> matrix);
 
-    int changeSize(int left, int right, int below, int avard);
+    int changeSize(int left, int right, int below, int above);
+    void rotate(RotationDirection rdir, int number);
 
     int addObject(std::size_t x, std::size_t y, GameObject gObj);
     int addObjectRandPos(GameObject gObj);
     void deteleObject(std::size_t x, std::size_t y);
-    std::vector<GameObject::Action> getPossibleActions(GameObject gObj);
 
-    const GameObject* getObjectAtPos(std::size_t x, std::size_t y);
-    const GameObject* getObjectByTag(std::string tag);
+    const std::vector<Cell> getAvailableCells() const;
 
-    class Cell {
-    public:
-        static std::unordered_set<std::string> possible_tags;
+    std::vector<const GameObject*> getObjectsAtPos(std::size_t x, std::size_t y) const;
+    std::pair<size_t, size_t> getPosOfObject(const GameObject& gObj) const;
 
-        bool isAvalForObj();
+    std::vector<const GameObject*> getObjectsByTag(std::string tag) const;
 
-    private:
-        std::unordered_set<std::string> tags;
-        bool is_aval_for_obj;
-    };
+    const std::vector<GameObject::Action> getObjectActions(GameObject gObj) const;
+    const std::vector<Action> getCellActions(std::size_t x, std::size_t y) const; // закончили здесь.
 
 private:
+
     std::vector<std::vector<Cell>> cells_matrix;
     std::vector<GameObject> objects;
     std::vector<GameObject> activeObjects;
     std::vector<GameObject> inactiveObjects;
+
 };
 
 #endif //UNIVERSAL_GAMING_FIELD_FIELD_H
